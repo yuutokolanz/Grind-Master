18e5b0a670e5ee992f4462b8e7e2b9639b79d5b8
Raul Ferreira da Rocha <raulferreiradarocha1@hotmail.com>
2025-05-12 18:43:06 -0300

    Adicionado Expo Router e roteamento básico

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+import React from "react";
+import { router } from "expo-router";
+
+export default function index() {
+  const handleLogin = () => {
+    console.log("TODO: login user!");
+    router.push("/(auth)/home");
+  };
+
+  const handleRegister = () => {
+    router.push("/register");
+  };
+
+  return (
+    <View>
+      <Text>Login</Text>
+
+      <Button
+      title="Login"
+      onPress={handleLogin} />
+
+      <Button
+      title="Register"
+      onPress={handleRegister} />
+    </View>
+  );
+}
